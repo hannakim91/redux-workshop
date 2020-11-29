80b53b9481538eb544ddb233a67237719678ae5b
Hanna Kim <hannakim91@gmail.com>
2020-11-28 17:11:55 -0700

    Comparison between react vs react/redux app

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -13,4 +13,8 @@ export const todos = (state = [], action) => {
 // fired when app launches - gives default values to store (global state)
 // ** all reducers fire when one is invoked -- why you need to give an initial value**
 // formatted as a condition: switch statement - default = initial state, case for what return value to change in global state - can be one other condition/case or can be multiple 
-// ADD_TODO == addIdea function in basic react app
+
+// react + redux app vs basic react app
+// ADD_TODO == addIdea function 
+// global state that can be accessed by App and all child components (when wrapped in Provider class) vs having to pass down props/methods to child, then grandchildren and so forth
+// both global state and class/functional component state need a value to initialize with - with redux, this is defined in a reducer's default value of switch statement
