68636b013ab7aac55ae1292cdf45da596c504216
Hanna Kim <hannakim91@gmail.com>
2020-11-28 18:04:55 -0700

    Create ToDoList Component

---
+++ src/containers/ToDoList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const ToDoList = ({todos}) => {
+  console.log(todos);
+
+  return (
+    <>
+      <ul>
+
+      </ul>
+    </>
+  )
+};
+
+const mapStateToProps = state => ({
+  todos: state.todos
+});
+ export default connect(mapStateToProps)(ToDoList)l
