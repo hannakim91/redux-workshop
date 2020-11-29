b6a568ec73fe5b7feb45682d9502e0127e35c8db
Hanna Kim <hannakim91@gmail.com>
2020-11-28 17:05:04 -0700

    More notes on reducers

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,3 +1,5 @@
 // reducers = functions that take 2 args
   // 1. an init/default value (like React component state)
   // 2. an action
+// fired when app launches - gives default values to store (global state)
+// ** all reducers fire when one is invoked -- why you need to give an initial value**
