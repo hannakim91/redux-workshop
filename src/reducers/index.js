1037956533bea27c57d9d5b5deec5a251696c166
Hanna Kim <hannakim91@gmail.com>
2020-11-28 17:17:14 -0700

    Note on combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,3 +6,5 @@ const rootReducer = combineReducers({
 });
 
 export default rootReducer;
+
+// function from redux library that creates one object from all reducers used in the app - takes in an object as an argument
