1d9a5853cb051274664197a1cb114e282faba8ad
Hanna Kim <hannakim91@gmail.com>
2020-11-28 17:18:06 -0700

    Another note on combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -9,3 +9,4 @@ export default rootReducer;
 
 // naming convention = rootReducer
 // function from redux library that creates one object from all reducers used in the app - takes in an object as an argument
+// properties of object return = each reducer
