7267680086730e36c806f780cb52b3c1716582fb
Hanna Kim <hannakim91@gmail.com>
2020-11-28 17:44:20 -0700

    Add notes on imported tools

--- src/index.js
+++ src/index.js
@@ -16,3 +16,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// Provider (react-redux library): makes Redux store available to any nested components - generally placed in top level (App)
+  // if React-Router also used - it also wraps around BrowserRouter
+// createStore (redux): creates Redux store that holds complete state tree of app - one per app
+// composeWithDevTools: pass as second arg of createStore to have access to devtools - same as window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() from other tut
