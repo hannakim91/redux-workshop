8164daf7f16a89ff26201b8a49a697c339487f2e
Hanna Kim <hannakim91@gmail.com>
2020-11-28 17:08:16 -0700

    Add note about switch statement usecase in reducers

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -12,3 +12,4 @@ export const todos = (state = [], action) => {
   // 2. an action
 // fired when app launches - gives default values to store (global state)
 // ** all reducers fire when one is invoked -- why you need to give an initial value**
+// formatted as a condition: switch statement - default = initial state, case for what return value to change in global state - can be one other condition/case or can be multiple 
