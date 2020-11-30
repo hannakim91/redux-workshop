1f770786ac296b93efdf9e09b04ea07841837801
Hanna Kim <hannakim91@gmail.com>
2020-11-29 17:40:08 -0700

    Create button elements

--- src/containers/ListFilters.js
+++ src/containers/ListFilters.js
@@ -6,6 +6,9 @@ const ListFilter = () => {
   return (
     <>
       <h3>Filter Your List</h3>
+      <button>Show All Tasks</button>
+      <button>Show Active</button>
+      <button>Show Completed</button>
     </>
   )
 }
