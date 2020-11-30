648bc635609724dbbf2d1d1b5f4b1dc5496b3dcf
Hanna Kim <hannakim91@gmail.com>
2020-11-29 17:39:13 -0700

    Add ListFilter component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import AddTodoForm from '../containers/AddTodoForm';
 import ToDoList from '../containers/ToDoList';
+import ListFilters from '../containers/ListFilters';
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <div className="App">
         <h1>Another Todo Box</h1>
         <AddTodoForm />
+        <ListFilters />
         <ToDoList />
       </div>
     );
---
+++ src/containers/ListFilters.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { markCompleted } from '../actions';
+
+const ListFilter = () => {
+  return (
+    <>
+      <h3>Filter Your List</h3>
+    </>
+  )
+}
+
+export default ListFilter;
